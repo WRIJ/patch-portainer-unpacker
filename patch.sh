108b580432398c9428081519877179dadd300c03
Jaap Roes <j.roes@wrij.nl>
2025-07-17 07:39:38 +0000

    Remove build script, replace it with patch script

--- patch.sh
+++ patch.sh
@@ -19,13 +19,6 @@ rm -rf "${COMPOSE_UNPACKER_DIR}" "${PORTAINER_DIR}"
 git clone --depth 1 --branch "${TAG}" git@github.com:portainer/portainer.git "${PORTAINER_DIR}"
 git clone --depth 1 --branch "${TAG}" git@github.com:portainer/compose-unpacker.git "${COMPOSE_UNPACKER_DIR}"
 
-# Determine the go version from the go.mod file
-GO_VERSION=$(grep -oP 'go \K([0-9]+\.?)+' "${COMPOSE_UNPACKER_DIR}/go.mod")
-if [ -z "${GO_VERSION}" ]; then
-  echo "Could not determine Go version from go.mod"
-  exit 1
-fi
-
 # Patch the go.mod file to use the mounted portainer directory
 patch -u "${COMPOSE_UNPACKER_DIR}/go.mod" patches/go.mod.patch
 
@@ -37,8 +30,3 @@ cp -r patches/webhooks/ "${COMPOSE_UNPACKER_DIR}/webhooks/"
 
 # Apply webhooks patch
 patch -u "${COMPOSE_UNPACKER_DIR}/commands/compose_deploy.go" patches/compose_deploy.go.patch
-
-docker build -t compose-unpacker:${TAG} \
-  --build-arg TAG="${TAG}" \
-  --build-arg GO_VERSION="${GO_VERSION}" \
-  .
